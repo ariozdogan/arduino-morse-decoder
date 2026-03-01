50ad339c40468fd236ed89b9e8becb7d93966a23
Ari Ozdogan <ariozdogan@Aris-MacBook-Air.local>
2026-02-28 20:42:59 -0500

    updating

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
 
+const int buttonPin = 8;
+const int ledPin = 7;
+
+void readUserInput(){
+  
+}
+
 
 
 void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 
 }
 
